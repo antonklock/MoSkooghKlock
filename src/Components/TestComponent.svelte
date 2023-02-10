a6fcf69a57e036ef70d4f3ad724bddeaf0627957
Anton Klock <klock.anton@gmail.com>
2023-02-10 15:41:14 +0100

    initial commit

---
+++ src/Components/TestComponent.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	const componentName = 'MoSkooghKlock';
+</script>
+
+<div>
+	<h3>Hello from {componentName}!</h3>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import TestComponent from '../Components/TestComponent.svelte';
+
+	const name = 'MoSkooghKlock';
+</script>
+
+<h1>Welcome to {name}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#each Array(3) as comp}
+	<TestComponent />
+{/each}
